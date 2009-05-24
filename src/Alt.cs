5a856d37b66065901416452bdbaacc707d71a506
snarfle <snarfle>
2009-05-24 17:52:09 +0000

    Add new entries for more broken interfaces.

--- src/Alt.cs
+++ src/Alt.cs
@@ -22,25 +22,98 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 #endregion
 
-// This entire file only exists to work around a bug in Media Foundation.  The core problem is
-// that there are a couple of interfaces that don't support QueryInterface.  AAR, .net won't marshal
-// parameters that contain instances of those classes.  To work around this, it is necessary to
-// change the definition to IntPtr for any method that touches those instances.
-//
-// Hopefully this will be fixed soon.
+// This entire file only exists to work around bugs in Media Foundation.  The core problem 
+// is that there are some objects in MF that don't correctly support QueryInterface.  In c++ 
+// this isn't a problem, since if you tell c++ that something is a pointer to an interface, 
+// it just believes you.  In fact, that's one of the places where c++ gets its performance:
+// it doesn't check anything.
+
+// In .Net, it checks.  And the way it checks is that every time it receives an interfaces
+// from unmanaged code, it does a couple of QI calls on it.  First it does a QI for IUnknown.
+// Second it does a QI for the specific interface it is supposed to be (ie IMFMediaSink or
+// whatever).
+
+// Since c++ *doesn't* check, oftentimes the first people to even try to call QI on some of 
+// MF's objects are c# programmers.  And, not surprisingly, sometimes the first time code is 
+// run, it doesn't work correctly.
+
+// The only way you can work around it is to change the definition of the method to from 
+// the IMFMediaSink (or whatever interface MF is trying to pass you) to IntPtr.  Of course, 
+// that limits what you can do with it.  You can't call methods on an IntPtr.
+
+// Something to keep in mind is that while the work-around involves changing the interface,
+// the problem isn't in the interface, it is in the object that implements the inteface.
+// This means that while the inteface may experience problems on one object, it may work
+// correctly on another object.  If you are unclear on the differences between an interface
+// and an object, it's time to hit the books.
+
+// In W7, MS has fixed a few of these issues that were reported in Vista.  The problem 
+// is that even if they are fixed in W7, if your program also needs to run on Vista, you 
+// still have to use the work-arounds.
+
+// I haven't started trying to use the new W7 intefaces, so I don't know what new problems
+// may have been introduced.
+
 
 using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Runtime.InteropServices;
+using System.Security;
 
 using MediaFoundation.Misc;
+using MediaFoundation.EVR;
 
-namespace MediaFoundation
+namespace MediaFoundation.Alt
 {
+    #region Bugs in Vista and W7
+
+    [ComImport, System.Security.SuppressUnmanagedCodeSecurity,
+    InterfaceType(ComInterfaceType.InterfaceIsIUnknown),
+    Guid("FA993888-4383-415A-A930-DD472A8CF6F7")]
+    public interface IMFGetServiceAlt
+    {
+        void GetService(
+            [In, MarshalAs(UnmanagedType.LPStruct)] Guid guidService,
+            [In, MarshalAs(UnmanagedType.LPStruct)] Guid riid,
+            out IntPtr ppvObject
+            );
+    }
+
+    [ComImport, System.Security.SuppressUnmanagedCodeSecurity,
+    Guid("FA993889-4383-415A-A930-DD472A8CF6F7"),
+    InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IMFTopologyServiceLookupAlt
+    {
+        void LookupService(
+            [In] MFServiceLookupType type,
+            [In] int dwIndex,
+            [In, MarshalAs(UnmanagedType.LPStruct)] Guid guidService,
+            [In, MarshalAs(UnmanagedType.LPStruct)] Guid riid,
+            [Out, MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.SysInt)] IntPtr[] ppvObjects,
+            [In, Out] ref int pnObjects
+            );
+    }
+
+    [ComImport, System.Security.SuppressUnmanagedCodeSecurity,
+    Guid("FA99388A-4383-415A-A930-DD472A8CF6F7"),
+    InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IMFTopologyServiceLookupClientAlt
+    {
+        void InitServicePointers(
+            IntPtr pLookup
+            );
+
+        void ReleaseServicePointers();
+    }
+
+    #endregion
+
+    #region Bugs in Vista that appear to be fixed in W7
+
     public class MFExternAlt
     {
-        [DllImport("MFPlat.dll", PreserveSig = false)]
+        [DllImport("MFPlat.dll", ExactSpelling = true, PreserveSig = false), SuppressUnmanagedCodeSecurity]
         public static extern void MFCreateEventQueue(
             out IMFMediaEventQueueAlt ppMediaEventQueue
         );
@@ -374,5 +447,7 @@ namespace MediaFoundation
             [In, MarshalAs(UnmanagedType.Interface)] IMFAsyncResult pResult
             );
     }
+
+    #endregion
 
 }

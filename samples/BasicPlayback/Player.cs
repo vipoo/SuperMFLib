2078c363394b68d8e6ac1bbc58041a1ee589982b
snarfle <snarfle>
2009-05-25 01:06:37 +0000

    Use MFRect

--- samples/BasicPlayback/Player.cs
+++ samples/BasicPlayback/Player.cs
@@ -240,7 +240,7 @@ class CPlayer : COMBase, IMFAsyncCallback
         {
             try
             {
-                RECT rcDest = new RECT();
+                MFRect rcDest = new MFRect();
                 MFVideoNormalizedRect nRect = new MFVideoNormalizedRect();
 
                 nRect.left = 0;

f1a8054d24737e51ee88922504e00430d7c49d21
snarfle <snarfle>
2012-03-15 02:18:41 +0000

    Add W8 interfaces and definitions.

--- src/MFIdl.cs
+++ src/MFIdl.cs
@@ -35,6 +35,13 @@ namespace MediaFoundation
 {
     #region GUIDs, IIDs
 
+    [UnmanagedName("CLSID_MFMediaEngineClassFactory"),
+    ComImport,
+    Guid("B44392DA-499B-446b-A4CB-005FEAD0E6D5")]
+    public class MFMediaEngineClassFactory
+    {
+    }
+
     [UnmanagedName("CLSID_HttpSchemePlugin"),
     ComImport,
     Guid("44cb442b-9da9-49df-b3fd-023777b16e50")]
@@ -75,6 +82,60 @@ namespace MediaFoundation
     Guid("9ec4b4f9-3029-45ad-947b-344de2a249e2")]
     public class UrlmonSchemePlugin
     {
+    }
+
+    public static class MF_MEDIA_ENGINE
+    {
+        // MF_MEDIA_ENGINE_CALLBACK
+        public static readonly Guid CALLBACK = new Guid(0xc60381b8, 0x83a4, 0x41f8, 0xa3, 0xd0, 0xde, 0x05, 0x07, 0x68, 0x49, 0xa9);
+
+        // MF_MEDIA_ENGINE_DXGI_MANAGER
+        public static readonly Guid DXGI_MANAGER = new Guid(0x065702da, 0x1094, 0x486d, 0x86, 0x17, 0xee, 0x7c, 0xc4, 0xee, 0x46, 0x48);
+
+        // MF_MEDIA_ENGINE_EXTENSION
+        public static readonly Guid EXTENSION = new Guid(0x3109fd46, 0x060d, 0x4b62, 0x8d, 0xcf, 0xfa, 0xff, 0x81, 0x13, 0x18, 0xd2);
+
+        // MF_MEDIA_ENGINE_PLAYBACK_HWND
+        public static readonly Guid PLAYBACK_HWND = new Guid(0xd988879b, 0x67c9, 0x4d92, 0xba, 0xa7, 0x6e, 0xad, 0xd4, 0x46, 0x03, 0x9d);
+
+        // MF_MEDIA_ENGINE_OPM_HWND
+        public static readonly Guid OPM_HWND = new Guid(0xa0be8ee7, 0x0572, 0x4f2c, 0xa8, 0x01, 0x2a, 0x15, 0x1b, 0xd3, 0xe7, 0x26);
+
+        // MF_MEDIA_ENGINE_PLAYBACK_VISUAL
+        public static readonly Guid PLAYBACK_VISUAL = new Guid(0x6debd26f, 0x6ab9, 0x4d7e, 0xb0, 0xee, 0xc6, 0x1a, 0x73, 0xff, 0xad, 0x15);
+
+        // MF_MEDIA_ENGINE_COREWINDOW
+        public static readonly Guid COREWINDOW = new Guid(0xfccae4dc, 0x0b7f, 0x41c2, 0x9f, 0x96, 0x46, 0x59, 0x94, 0x8a, 0xcd, 0xdc);
+
+        // MF_MEDIA_ENGINE_VIDEO_OUTPUT_FORMAT
+        public static readonly Guid VIDEO_OUTPUT_FORMAT = new Guid(0x5066893c, 0x8cf9, 0x42bc, 0x8b, 0x8a, 0x47, 0x22, 0x12, 0xe5, 0x27, 0x26);
+
+        // MF_MEDIA_ENGINE_CONTENT_PROTECTION_FLAGS
+        public static readonly Guid CONTENT_PROTECTION_FLAGS = new Guid(0xe0350223, 0x5aaf, 0x4d76, 0xa7, 0xc3, 0x06, 0xde, 0x70, 0x89, 0x4d, 0xb4);
+
+        // MF_MEDIA_ENGINE_CONTENT_PROTECTION_MANAGER
+        public static readonly Guid CONTENT_PROTECTION_MANAGER = new Guid(0xfdd6dfaa, 0xbd85, 0x4af3, 0x9e, 0x0f, 0xa0, 0x1d, 0x53, 0x9d, 0x87, 0x6a);
+
+        // MF_MEDIA_ENGINE_AUDIO_ENDPOINT_ROLE
+        public static readonly Guid AUDIO_ENDPOINT_ROLE = new Guid(0xd2cb93d1, 0x116a, 0x44f2, 0x93, 0x85, 0xf7, 0xd0, 0xfd, 0xa2, 0xfb, 0x46);
+
+        // MF_MEDIA_ENGINE_AUDIO_CATEGORY
+        public static readonly Guid AUDIO_CATEGORY = new Guid(0xc8d4c51d, 0x350e, 0x41f2, 0xba, 0x46, 0xfa, 0xeb, 0xbb, 0x08, 0x57, 0xf6);
+
+        // MF_MEDIA_ENGINE_STREAM_CONTAINS_ALPHA_CHANNEL
+        public static readonly Guid STREAM_CONTAINS_ALPHA_CHANNEL = new Guid(0x5cbfaf44, 0xd2b2, 0x4cfb, 0x80, 0xa7, 0xd4, 0x29, 0xc7, 0x4c, 0x78, 0x9d);
+
+        // MF_MEDIA_ENGINE_BROWSER_COMPATIBILITY_MODE
+        public static readonly Guid BROWSER_COMPATIBILITY_MODE = new Guid(0x4e0212e2, 0xe18f, 0x41e1, 0x95, 0xe5, 0xc0, 0xe7, 0xe9, 0x23, 0x5b, 0xc3);
+
+        // MF_MEDIA_ENGINE_BROWSER_COMPATIBILITY_MODE_IE9
+        public static readonly Guid BROWSER_COMPATIBILITY_MODE_IE9 = new Guid(0x052c2d39, 0x40c0, 0x4188, 0xab, 0x86, 0xf8, 0x28, 0x27, 0x3b, 0x75, 0x22);
+
+        // MF_MEDIA_ENGINE_BROWSER_COMPATIBILITY_MODE_IE10
+        public static readonly Guid BROWSER_COMPATIBILITY_MODE_IE10 = new Guid(0x11a47afd, 0x6589, 0x4124, 0xb3, 0x12, 0x61, 0x58, 0xec, 0x51, 0x7f, 0xc3);
+
+        // MF_MEDIA_ENGINE_SOURCE_RESOLVER_CONFIG_STORE
+        public static readonly Guid SOURCE_RESOLVER_CONFIG_STORE = new Guid(0x0ac0c497, 0xb3c4, 0x48c9, 0x9c, 0xde, 0xbb, 0x8c, 0xa2, 0x44, 0x2c, 0xa3);
     }
 
     public static class MFAttributesClsid

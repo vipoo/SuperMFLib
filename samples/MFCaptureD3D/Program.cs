daae6bb840ce8290a4418a6e91bc23b458c387b8
snarfle <snarfle>
2012-04-19 23:14:54 +0000

    New sample

---
+++ samples/MFCaptureD3D/Program.cs
@@ -0,0 +1,32 @@
+/****************************************************************************
+While the underlying libraries are covered by LGPL, this sample is released
+as public domain.  It is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+Written by:
+Gerardo Hernandez
+BrightApp.com
+
+Modified by snarfle
+*****************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace MFCaptureD3D
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [MTAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new WinMain());
+        }
+    }
+}
---
+++ samples/MFCaptureD3D/Properties/AssemblyInfo.cs
@@ -0,0 +1,50 @@
+﻿/****************************************************************************
+While the underlying libraries are covered by LGPL, this sample is released
+as public domain.  It is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+Written by:
+Gerardo Hernandez
+BrightApp.com
+
+Modified by snarfle
+*****************************************************************************/
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: NeutralResourcesLanguageAttribute("en")]
+[assembly: AssemblyTitle("MFCaptureD3D")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MFCaptureD3D")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8eab3471-f6c9-4dd4-8a33-a21a4469ada0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

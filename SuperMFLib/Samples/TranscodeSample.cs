7fc0e230c2eb795124dad2bb816a44f9421dc314
Dean Netherton <antispam@netherton.org>
2014-05-18 13:13:28 +1000

    Moved from iRacingReplay app, the sample processors: MediaTypeChange, SaveTo and SeperateAudioVideo.  Also create an initial sample application for the supermflib

---
+++ SuperMFLib/Samples/TranscodeSample.cs
@@ -0,0 +1,134 @@
+﻿using MediaFoundation;
+using MediaFoundation.Net;
+using MediaFoundation.Transform;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Samples
+{
+    class TranscodeSample
+    {
+        const string sourceFile = @"C:\Users\dean\Documents\Bandicam\bandicam 2014-05-18 11-22-12-572.avi";
+        const string destinationFile = @"C:\Users\dean\Documents\Bandicam\sample.wmv";
+        const int VideoBitRate = 6*1000000;
+        const int AudioBitRate = 48000;
+
+        static Guid TARGET_AUDIO_FORMAT = MFMediaType.WMAudioV9;
+        static Guid TARGET_VIDEO_FORMAT = MFMediaType.WMV3;
+
+        public void Process()
+        {
+
+            using (MFSystem.Start())
+            {
+                var readWriteFactory = new ReadWriteClassFactory();
+
+                var attributes = new Attributes
+                {
+                    ReadWriterEnableHardwareTransforms = true,
+                    SourceReaderEnableVideoProcessing = true
+                };
+
+
+                var sourceReader = readWriteFactory.CreateSourceReaderFromURL(sourceFile, attributes);
+                var sinkWriter = readWriteFactory.CreateSinkWriterFromURL(destinationFile, attributes);
+
+                var writeToSink = ConnectStreams(sourceReader, sinkWriter);
+
+                using (sinkWriter.BeginWriting())
+                {
+                    sourceReader.Samples( sample => 
+                        {
+                            Console.Clear();
+                            Console.WriteLine(TimeSpan.FromSeconds(sample.Timestamp.FromNanoToSeconds()));
+
+                            return writeToSink(sample);
+                        });
+                }
+            }
+
+        }
+
+        private ProcessSample ConnectStreams(SourceReader sourceReader, SinkWriter sinkWriter)
+        {
+            var sourceAudioStream = SetAudioMediaType(sourceReader);
+            var sourceVideoStream = SetVideoMediaType(sourceReader);
+
+            var sinkAudioStream = AddStream(sinkWriter, sourceAudioStream.CurrentMediaType, CreateTargetAudioMediaType(sourceAudioStream.NativeMediaType));
+            var sinkVideoStream = AddStream(sinkWriter, sourceVideoStream.CurrentMediaType, CreateTargetVideoMediaType(sourceVideoStream.NativeMediaType));
+
+            var saveAudio = AVOperations.MediaTypeChange(sinkAudioStream, AVOperations.SaveTo(sinkAudioStream));
+            var saveVideo = AVOperations.MediaTypeChange(sinkVideoStream, AVOperations.SaveTo(sinkVideoStream));
+
+            return AVOperations.SeperateAudioVideo(saveAudio, saveVideo);
+        }
+
+        SourceStream SetAudioMediaType(SourceReader sourceReader)
+        {
+            var sourceStream = sourceReader.Streams.First(s => s.IsSelected && s.NativeMediaType.IsAudio);
+
+            sourceStream.CurrentMediaType = new MediaType() { MajorType = MFMediaType.Audio, SubType = MFMediaType.PCM };
+
+            return sourceStream;
+        }
+
+        SourceStream SetVideoMediaType(SourceReader sourceReader)
+        {
+            var sourceStream = sourceReader.Streams.First(s => s.IsSelected && s.NativeMediaType.IsVideo);
+
+            sourceStream.CurrentMediaType = new MediaType() { MajorType = MFMediaType.Video, SubType = MFMediaType.RGB32 };
+
+            return sourceStream;
+        }
+
+        SinkStream AddStream(SinkWriter sinkWriter, MediaType input, MediaType encoding)
+        {
+            var sinkStream = sinkWriter.AddStream(encoding);
+            sinkStream.InputMediaType = input;
+            return sinkStream;
+        }
+
+        MediaType CreateTargetAudioMediaType(MediaType nativeMediaType)
+        {
+            var numberOfChannels = nativeMediaType.AudioNumberOfChannels;
+            var sampleRate = nativeMediaType.AudioSamplesPerSecond;
+
+            var availableTypes = MFSystem.TranscodeGetAudioOutputAvailableTypes(TARGET_AUDIO_FORMAT, MFT_EnumFlag.All);
+
+            var type = availableTypes
+                .FirstOrDefault(t => t.AudioNumberOfChannels == numberOfChannels &&
+                    t.AudioSamplesPerSecond == sampleRate &&
+                    t.AudioAverageBytesPerSecond == AudioBitRate);
+
+            if (type != null)
+                return new MediaType(type);
+
+            throw new Exception("Unable to find target audio format");
+        }
+
+        MediaType CreateTargetVideoMediaType(MediaType nativeMediaType)
+        {
+            var size = nativeMediaType.FrameSize;
+            var rate = nativeMediaType.FrameRate;
+            var aspect = nativeMediaType.AspectRatio;
+            var bitRate = VideoBitRate;
+
+            var mediaType = new MediaType()
+            {
+                MajorType = MFMediaType.Video,
+                SubType = TARGET_VIDEO_FORMAT,
+                FrameSize = size,
+                FrameRate = rate,
+                AspectRatio = aspect,
+                BitRate = bitRate,
+                InterlaceMode = MFVideoInterlaceMode.Progressive
+            };
+
+            return mediaType;
+        }
+
+    }
+}

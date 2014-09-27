5a460a14d8bffaafb16e73527355049c70c51ac6
Dean Netherton <dean.netherton@gmail.com>
2014-09-27 14:16:46 +1000

    Whitespace cleanup

--- SuperMFLib/SourceStream.cs
+++ SuperMFLib/SourceStream.cs
@@ -23,65 +23,63 @@ using System.Collections.Generic;
 
 namespace MediaFoundation.Net
 {
-	public struct SourceStream
-	{
-		public readonly SourceReader sourceReader;
-		public readonly int index;
+    public struct SourceStream
+    {
+        public readonly SourceReader sourceReader;
+        public readonly int index;
 
-		public SourceStream(SourceReader sourceReader, int index)
-		{
-			this.sourceReader = sourceReader;
-			this.index = index;
-		}
-			
-		public bool IsSelected
-		{
-			get
-			{
-				bool result;
-				sourceReader.instance.GetStreamSelection (index, out result).Hr ();
-				return result;
-			}
-		}
+        public SourceStream(SourceReader sourceReader, int index)
+        {
+            this.sourceReader = sourceReader;
+            this.index = index;
+        }
 
-		public MediaType CurrentMediaType
-		{
-			get
-			{
-				IMFMediaType ppMediaType;
+        public bool IsSelected
+        {
+            get
+            {
+                bool result;
+                sourceReader.instance.GetStreamSelection(index, out result).Hr();
+                return result;
+            }
+        }
 
-				sourceReader.instance.GetCurrentMediaType (index, out ppMediaType).Hr ();
+        public MediaType CurrentMediaType
+        {
+            get
+            {
+                IMFMediaType ppMediaType;
 
-				return new MediaType(ppMediaType);
-			}
-			set
-			{
-				sourceReader.instance.SetCurrentMediaType (index, IntPtr.Zero, value.instance).Hr ();
-			}
-		}
+                sourceReader.instance.GetCurrentMediaType(index, out ppMediaType).Hr();
 
-		public MediaType NativeMediaType
-		{
-			get
-			{
-				IMFMediaType ppMediaType;
+                return new MediaType(ppMediaType);
+            }
+            set
+            {
+                sourceReader.instance.SetCurrentMediaType(index, IntPtr.Zero, value.instance).Hr();
+            }
+        }
 
-				sourceReader.instance.GetNativeMediaType (index, 0, out ppMediaType).Hr ();
+        public MediaType NativeMediaType
+        {
+            get
+            {
+                IMFMediaType ppMediaType;
 
-				return new MediaType(ppMediaType);
-			}
-		}
+                sourceReader.instance.GetNativeMediaType(index, 0, out ppMediaType).Hr();
 
-		public ulong Duration
-		{
-			get
-			{
-				PropVariant result = new PropVariant ();
-				sourceReader.instance.GetPresentationAttribute (index, MFAttributesClsid.MF_PD_DURATION, result).Hr ();
-				return result.GetULong ();
-			}
-		}
-	}
+                return new MediaType(ppMediaType);
+            }
+        }
 
-
+        public ulong Duration
+        {
+            get
+            {
+                PropVariant result = new PropVariant();
+                sourceReader.instance.GetPresentationAttribute(index, MFAttributesClsid.MF_PD_DURATION, result).Hr();
+                return result.GetULong();
+            }
+        }
+    }
 }

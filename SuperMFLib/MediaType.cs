64d7cb0a1eb36d608035566274aa8b5787edc45a
Dean Netherton <dean.netherton@gmail.com>
2014-10-11 09:29:46 +1100

    Tabs to spaces

--- SuperMFLib/MediaType.cs
+++ SuperMFLib/MediaType.cs
@@ -22,173 +22,173 @@ using System.Drawing;
 
 namespace MediaFoundation.Net
 {
-	public class MediaType : COMDisposable<IMFMediaType>, IDisposable
+    public class MediaType : COMDisposable<IMFMediaType>, IDisposable
     {
-		public MediaType(IMFMediaType instance) : base(instance) 	{ }
+        public MediaType(IMFMediaType instance) : base(instance) { }
 
-		public MediaType () : base(MediaType.NewInstance())			{ }
+        public MediaType() : base(MediaType.NewInstance()) { }
 
-		public MediaType (MediaType type) : base(CopyFrom(type))	{ }
-			
-		private static IMFMediaType NewInstance()
-		{
-			IMFMediaType result;
-			MFExtern.MFCreateMediaType(out result).Hr();
-			return result;
-		}
+        public MediaType(MediaType type) : base(CopyFrom(type)) { }
 
-		private static IMFMediaType CopyFrom(MediaType type)
-		{
-			IMFMediaType result;
-			MFExtern.MFCreateMediaType(out result).Hr();
-			type.instance.CopyAllItems(result);
+        private static IMFMediaType NewInstance()
+        {
+            IMFMediaType result;
+            MFExtern.MFCreateMediaType(out result).Hr();
+            return result;
+        }
 
-			return result;
-		}
+        private static IMFMediaType CopyFrom(MediaType type)
+        {
+            IMFMediaType result;
+            MFExtern.MFCreateMediaType(out result).Hr();
+            type.instance.CopyAllItems(result);
 
-		public Guid MajorType
-		{
-			get { return GetGuid (MFAttributesClsid.MF_MT_MAJOR_TYPE);	}
-			set	{ SetGuid (MFAttributesClsid.MF_MT_MAJOR_TYPE, value); }
-		}
+            return result;
+        }
 
-		public Guid SubType
-		{
-			get	{ return GetGuid (MFAttributesClsid.MF_MT_SUBTYPE); }
-			set	{ SetGuid (MFAttributesClsid.MF_MT_SUBTYPE, value); }
-		}
+        public Guid MajorType
+        {
+            get { return GetGuid(MFAttributesClsid.MF_MT_MAJOR_TYPE); }
+            set { SetGuid(MFAttributesClsid.MF_MT_MAJOR_TYPE, value); }
+        }
 
-		public bool IsAudio
-		{
-			get	{ return MajorType == MFMediaType.Audio; }
-		}
+        public Guid SubType
+        {
+            get { return GetGuid(MFAttributesClsid.MF_MT_SUBTYPE); }
+            set { SetGuid(MFAttributesClsid.MF_MT_SUBTYPE, value); }
+        }
 
-		public bool IsVideo
-		{
-			get	{ return MajorType == MFMediaType.Video; }
-		}
+        public bool IsAudio
+        {
+            get { return MajorType == MFMediaType.Audio; }
+        }
 
-		public int AudioNumberOfChannels
-		{
-			get { return GetInt (MFAttributesClsid.MF_MT_AUDIO_NUM_CHANNELS); }
-		}
+        public bool IsVideo
+        {
+            get { return MajorType == MFMediaType.Video; }
+        }
 
-		public int AudioSamplesPerSecond
-		{
-			get	{ return GetInt (MFAttributesClsid.MF_MT_AUDIO_SAMPLES_PER_SECOND);	}
-		}
+        public int AudioNumberOfChannels
+        {
+            get { return GetInt(MFAttributesClsid.MF_MT_AUDIO_NUM_CHANNELS); }
+        }
+
+        public int AudioSamplesPerSecond
+        {
+            get { return GetInt(MFAttributesClsid.MF_MT_AUDIO_SAMPLES_PER_SECOND); }
+        }
 
         public int AudioAverageBytesPerSecond
         {
             get { return GetInt(MediaFoundation.MFAttributesClsid.MF_MT_AUDIO_AVG_BYTES_PER_SECOND); }
         }
 
-		public int BitRate
-		{
-			get { return GetInt (MFAttributesClsid.MF_MT_AVG_BITRATE); }
-			set { SetInt (MFAttributesClsid.MF_MT_AVG_BITRATE, value); }
-		}
+        public int BitRate
+        {
+            get { return GetInt(MFAttributesClsid.MF_MT_AVG_BITRATE); }
+            set { SetInt(MFAttributesClsid.MF_MT_AVG_BITRATE, value); }
+        }
 
         public bool TryGetBitRate(out int bitRate)
         {
             return instance.GetUINT32(MFAttributesClsid.MF_MT_AVG_BITRATE, out bitRate) == 0;
         }
 
-		public MFVideoInterlaceMode InterlaceMode
-		{
-			get
-			{
-				return (MFVideoInterlaceMode)GetInt (MFAttributesClsid.MF_MT_INTERLACE_MODE);
-			}
-			set
-			{
-				SetInt (MFAttributesClsid.MF_MT_INTERLACE_MODE, (int)value);
-			}
-		}
+        public MFVideoInterlaceMode InterlaceMode
+        {
+            get
+            {
+                return (MFVideoInterlaceMode)GetInt(MFAttributesClsid.MF_MT_INTERLACE_MODE);
+            }
+            set
+            {
+                SetInt(MFAttributesClsid.MF_MT_INTERLACE_MODE, (int)value);
+            }
+        }
 
-		public Size FrameSize
-		{
-			get
-			{
-				var result = GetLong(MFAttributesClsid.MF_MT_FRAME_SIZE);
+        public Size FrameSize
+        {
+            get
+            {
+                var result = GetLong(MFAttributesClsid.MF_MT_FRAME_SIZE);
 
-				return new Size (result.HighPart(), result.LowPart());
-			}
-			set
-			{
-				var size = LongEx.FromInts (value.Width, value.Height);
+                return new Size(result.HighPart(), result.LowPart());
+            }
+            set
+            {
+                var size = LongEx.FromInts(value.Width, value.Height);
 
-				SetLong (MFAttributesClsid.MF_MT_FRAME_SIZE, size);
-			}
-		}
+                SetLong(MFAttributesClsid.MF_MT_FRAME_SIZE, size);
+            }
+        }
 
-		public FrameRate FrameRate
-		{
-			get
-			{
-				var result = GetLong(MFAttributesClsid.MF_MT_FRAME_RATE);
+        public FrameRate FrameRate
+        {
+            get
+            {
+                var result = GetLong(MFAttributesClsid.MF_MT_FRAME_RATE);
 
-				return new FrameRate (result.HighPart(), result.LowPart());
-			}
-			set
-			{
-				var size = LongEx.FromInts (value.Numerator, value.Denominator);
+                return new FrameRate(result.HighPart(), result.LowPart());
+            }
+            set
+            {
+                var size = LongEx.FromInts(value.Numerator, value.Denominator);
 
-				SetLong (MFAttributesClsid.MF_MT_FRAME_RATE, size);
-			}
-		}
+                SetLong(MFAttributesClsid.MF_MT_FRAME_RATE, size);
+            }
+        }
 
-		public AspectRatio AspectRatio
-		{
-			get
-			{
-				var result = GetLong(MFAttributesClsid.MF_MT_PIXEL_ASPECT_RATIO);
+        public AspectRatio AspectRatio
+        {
+            get
+            {
+                var result = GetLong(MFAttributesClsid.MF_MT_PIXEL_ASPECT_RATIO);
 
-				return new AspectRatio (result.HighPart(), result.LowPart());
-			}
-			set
-			{
-				var size = LongEx.FromInts (value.XAspect, value.YAspect);
+                return new AspectRatio(result.HighPart(), result.LowPart());
+            }
+            set
+            {
+                var size = LongEx.FromInts(value.XAspect, value.YAspect);
 
-				SetLong (MFAttributesClsid.MF_MT_PIXEL_ASPECT_RATIO, size);
-			}
-		}
+                SetLong(MFAttributesClsid.MF_MT_PIXEL_ASPECT_RATIO, size);
+            }
+        }
 
 
-		public Guid GetGuid(Guid guid)
-		{
-			Guid result;
-			instance.GetGUID (guid, out result).Hr();
-			return result;
-		}
+        public Guid GetGuid(Guid guid)
+        {
+            Guid result;
+            instance.GetGUID(guid, out result).Hr();
+            return result;
+        }
 
-		public void SetGuid(Guid guid, Guid value)
-		{
-			instance.SetGUID (guid, value).Hr();
-		}
+        public void SetGuid(Guid guid, Guid value)
+        {
+            instance.SetGUID(guid, value).Hr();
+        }
 
-		public int GetInt(Guid guid)
-		{
-			int result;
-			instance.GetUINT32 (guid, out result).Hr ();
-			return result;
-		}
+        public int GetInt(Guid guid)
+        {
+            int result;
+            instance.GetUINT32(guid, out result).Hr();
+            return result;
+        }
 
-		public void SetInt(Guid guid, int value)
-		{
-			instance.SetUINT32 (guid, value).Hr ();;
-		}
+        public void SetInt(Guid guid, int value)
+        {
+            instance.SetUINT32(guid, value).Hr(); ;
+        }
 
-		public long GetLong(Guid guid)
-		{
-			long result;
-			instance.GetUINT64 (guid, out result).Hr ();
-			return result;
-		}
+        public long GetLong(Guid guid)
+        {
+            long result;
+            instance.GetUINT64(guid, out result).Hr();
+            return result;
+        }
 
-		public void SetLong(Guid guid, long value)
-		{
-			instance.SetUINT64 (guid, value).Hr();
-		}
+        public void SetLong(Guid guid, long value)
+        {
+            instance.SetUINT64(guid, value).Hr();
+        }
     }
 }

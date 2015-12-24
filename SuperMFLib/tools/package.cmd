a4697b8110735fc78c2da82f7ab337de607096ac
Dean Netherton <dean.netherton@gmail.com>
2015-12-24 13:07:48 +1100

    Moved merging of assembly from afterBuild step, to only happen prior to nuget packing

--- SuperMFLib/tools/package.cmd
+++ SuperMFLib/tools/package.cmd
@@ -3,4 +3,8 @@ SET ProjectDir=%~dp0..\
 
 msbuild %ProjectDir%SuperMFLib.csproj -p:SolutionDir=%SolutionDir% -t:rebuild -p:ApplicationVersion=%APPVEYOR_BUILD_VERSION% -v:minimal /p:Configuration=Release
 
+copy %ProjectDir%bin\x64\Release\MediaFoundation.Net.dll %ProjectDir%bin\x64\Release\MediaFoundation.Net.unmerged.dll 
+
+%SolutionDir%packages\ILMerge.2.14.1208\tools\ILMerge.exe /targetplatform:v2,C:\Windows\Microsoft.NET\Framework\v2.0.50727 /out:%ProjectDir%bin\x64\Release\MediaFoundation.Net.dll %ProjectDir%bin\x64\Release\MediaFoundation.Net.unmerged.dll %ProjectDir%bin\x64\Release\MediaFoundation.dll
+
 %ProjectDir%tools\nuget.exe pack %ProjectDir%SuperMFLib.nuspec -Prop Configuration=release -Verbosity detail -Version %APPVEYOR_BUILD_VERSION%
